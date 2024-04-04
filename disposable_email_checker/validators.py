61d941ac97540ebc9e16f08dadd026affaea3dfb
Justin Gill <gill.justin1999@gmail.com>
2024-04-04 13:04:53 -0700

    ugettext -> gettext

--- disposable_email_checker/validators.py
+++ disposable_email_checker/validators.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.utils.encoding import force_str
 from django.core import validators
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from bdea.client import BDEAClient
 from django.urls import get_callable
